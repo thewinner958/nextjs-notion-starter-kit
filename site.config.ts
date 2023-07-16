140cf82f899b9ef6cb4743b8d7fe83031c3a3502
TheWinner <thewinner958@gmail.com>
2023-07-16 23:21:53 +0300

    filled the necessary config

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6a6d77ac482549c2bc36c7e671c85f00',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'TheWinner\'s Blog',
+  domain: 'winners-blog.vercel.com',
+  author: 'TheWinner',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'This is my blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  twitter: 'thewinner958',
+  github: 'thewinner958',
+  //linkedin: 'fisch2',
+  mastodon: 'https://ohai.social/@thewinner958', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
